e2894f43e55b1774e4ef0f1fca18dfa2b13e950c
ahmetkoprulu <aensarkoprulu@gmail.com>
2022-06-29 09:31:46 +0300

    Improved bezier path

--- src/utils/svg.js
+++ src/utils/svg.js
@@ -5,8 +5,6 @@ export const getCenter = ({
   sourceY,
   targetX,
   targetY,
-  sourcePosition = "right",
-  targetPosition = "left",
   constantOffset = null
 }) => {
   const xOffset = constantOffset || Math.abs(targetX - sourceX) / 2
@@ -21,31 +19,23 @@ export const getCenter = ({
 export function getBezierPath({
   sourceX,
   sourceY,
-  sourcePosition = "right",
   targetX,
   targetY,
-  targetPosition = "left",
   centerX,
   centerY,
 }) {
-  const [_centerX, _centerY] = getCenter({ sourceX, sourceY, targetX, targetY })
-
-  const leftAndRight = ["left", "right"]
+  const [_centerX, _centerY, offsetX, offsetY] = getCenter({ sourceX, sourceY, targetX, targetY })
 
   const cX = typeof centerX !== 'undefined' ? centerX : _centerX
   const cY = typeof centerY !== 'undefined' ? centerY : _centerY
-
-  let path = `M${sourceX},${sourceY} C${sourceX},${cY} ${targetX},${cY} ${targetX},${targetY}`
-
-  if (leftAndRight.includes(sourcePosition) && leftAndRight.includes(targetPosition)) {
-    path = `M${sourceX},${sourceY} C${cX},${sourceY} ${cX},${targetY} ${targetX},${targetY}`
-  } else if (leftAndRight.includes(targetPosition)) {
-    path = `M${sourceX},${sourceY} Q${sourceX},${targetY} ${sourceX},${targetY} ${targetX},${targetY}`
-  } else if (leftAndRight.includes(sourcePosition)) {
-    path = `M${sourceX},${sourceY} Q${targetX},${sourceY} ${targetX},${sourceY} ${targetX},${targetY}`
+  if (sourceX <= targetX) {
+    return `M${sourceX},${sourceY} C${cX},${sourceY} ${cX},${targetY} ${targetX},${targetY}`
   }
 
-  return path
+  if (sourceY <= targetY) {
+    return `M${sourceX},${sourceY} C${sourceX + 20},${sourceY + 30} ${sourceX + 20},${sourceY + 30} ${sourceX - offsetX},${sourceY + offsetY} S ${targetX - 20} ${targetY - 30} ${targetX},${targetY}`
+  }
+  return `M${sourceX},${sourceY} C${sourceX + 20},${sourceY - 30} ${sourceX + 20},${sourceY - 30} ${sourceX - offsetX},${sourceY - offsetY} S ${targetX - 20} ${targetY + 30} ${targetX},${targetY}`
 }
 
 export const bottomLeftCorner = (x, y, size) => `L ${x},${y - size}Q ${x},${y} ${x + size},${y}`
@@ -60,10 +50,8 @@ export const rightTopCorner = (x, y, size) => `L ${x - size},${y}Q ${x},${y} ${x
 export function getSmoothStepPath({
   sourceX,
   sourceY,
-  sourcePosition = "right",
   targetX,
   targetY,
-  targetPosition = "left",
   borderRadius = 5,
   centerX,
   centerY,
@@ -72,7 +60,7 @@ export function getSmoothStepPath({
   const cornerWidth = Math.min(borderRadius, Math.abs(targetX - sourceX))
   const cornerHeight = Math.min(borderRadius, Math.abs(targetY - sourceY))
   const cornerSize = Math.min(cornerWidth, cornerHeight, offsetX, offsetY)
-  const leftAndRight = ["left", "right"]
+
   const cX = typeof centerX !== 'undefined' ? centerX : _centerX
   const cY = typeof centerY !== 'undefined' ? centerY : _centerY
 
@@ -84,24 +72,19 @@ export function getSmoothStepPath({
     secondCornerPath = sourceY <= targetY ? bottomLeftCorner(cX, targetY, cornerSize) : topLeftCorner(cX, targetY, cornerSize)
 
     return `M ${sourceX},${sourceY}${firstCornerPath}${secondCornerPath}L ${targetX},${targetY}`
-  } else {
-    let firstLine;
-    if (sourceY <= targetY) {
-      firstCornerPath = `${bottomRightCorner(sourceX + 20, sourceY + offsetY, cornerSize)} ${rightBottomCorner(sourceX + 20 - offsetX, sourceY + offsetY, 0)}`
-      secondCornerPath = `${leftTopCorner(sourceX - offsetX * 2 - 20, sourceY + offsetY, cornerSize)} ${bottomLeftCorner(sourceX - offsetX * 2 - 20, targetY, cornerSize)}`
-      firstLine = rightTopCorner(sourceX + 20, sourceY, cornerSize)
-    }
-    else {
-      firstCornerPath = `${topRightCorner(sourceX + 20, sourceY - offsetY, cornerSize)} ${rightBottomCorner(sourceX + 20 - offsetX, sourceY - offsetY, 0)}`
-      secondCornerPath = `${leftBottomCorner(sourceX - offsetX * 2 - 20, sourceY - offsetY, cornerSize)} ${topLeftCorner(sourceX - offsetX * 2 - 20, targetY, cornerSize)}`
-      firstLine = rightBottomCorner(sourceX + 20, sourceY, cornerSize)
-    }
-
-    return `M ${sourceX},${sourceY}, ${firstLine} ${firstCornerPath} ${secondCornerPath} L ${targetX},${targetY}`
-
-    // return `M ${sourceX},${sourceY}, L ${sourceX + 20},${sourceY}  L ${sourceX + 20},${sourceY - offsetY}  L ${sourceX + 20 - offsetX},${sourceY - offsetY}`
-    // return `M ${sourceX},${sourceY}${firstCornerPath}${secondCornerPath}L ${targetX},${targetY}`
-
   }
 
+  let firstLine;
+  if (sourceY <= targetY) {
+    firstCornerPath = `${bottomRightCorner(sourceX + 20, sourceY + offsetY, cornerSize)} ${rightBottomCorner(sourceX + 20 - offsetX, sourceY + offsetY, 0)}`
+    secondCornerPath = `${leftTopCorner(sourceX - offsetX * 2 - 20, sourceY + offsetY, cornerSize)} ${bottomLeftCorner(sourceX - offsetX * 2 - 20, targetY, cornerSize)}`
+    firstLine = rightTopCorner(sourceX + 20, sourceY, cornerSize)
+  }
+  else {
+    firstCornerPath = `${topRightCorner(sourceX + 20, sourceY - offsetY, cornerSize)} ${rightBottomCorner(sourceX + 20 - offsetX, sourceY - offsetY, 0)}`
+    secondCornerPath = `${leftBottomCorner(sourceX - offsetX * 2 - 20, sourceY - offsetY, cornerSize)} ${topLeftCorner(sourceX - offsetX * 2 - 20, targetY, cornerSize)}`
+    firstLine = rightBottomCorner(sourceX + 20, sourceY, cornerSize)
+  }
+
+  return `M ${sourceX},${sourceY}, ${firstLine} ${firstCornerPath} ${secondCornerPath} L ${targetX},${targetY}`
 }
